1d7a0ffa0e9fc79adcef8e43ff3d9744db986ec1
Chad Altemose <chad@chadzilla.com>
2015-01-11 22:13:59 -0500

    added basic sorting and stub method for finding by date.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,8 @@
 module.exports = function (app, db) {
 
+    // get the weather collection
+    var weather = db.collection("weather");
+
     app.get('/api', function(req, res) {
         // @TODO turn this into documentation of api functions?
         res.send('GET /api');
@@ -7,7 +10,7 @@ module.exports = function (app, db) {
 
     app.get('/api/weather', function (req, res) {
         // return most recent weather sorted by date
-        var weather = db.collection("weather");
+        // var weather = db.collection("weather");
         weather.find({}).sort({date:-1}).toArray(function(err, documents) {
             if (err) res.jsonp({err:err});
             else res.jsonp({result: documents});
@@ -15,26 +18,31 @@ module.exports = function (app, db) {
     });
 
     app.post('/api/weather', function (req, res) {
-        console.log('/api/weather POST', req.body);
+        // add/overwrite date here since Tessel dates cannot be trusted
+        req.body.date = new Date();
 
-        // require a date property
-        if (!req.body.date) res.jsonp({err:'You must supply a date property.'});
-
-        // reformat the date string to a date obj
-        req.body.date = new Date(req.body.date);
-        
-        // get the weather collection
-        var collection = db.collection("weather");
+        console.log(req.body.humid.toFixed(1) + '%RH', req.body.temp.f.toFixed(1) + 'F', req.body.temp.c.toFixed(1) + 'C', req.body.date.toLocaleString());
         
         // insert req.body into weather collection
-        collection.insert(req.body, function (err, result) {
+        weather.insert(req.body, function (err, result) {
             // handle error results
             if (err) return res.jsonp({err:err});
             if (!result) return res.jsonp({err:'No data inserted into database.'});
             
             // return results
-            console.log(result[0]);
             res.jsonp({result:result[0]});
         });
+    });
+
+    app.get('/api/weather/testing', function (req, res) {
+        // return some recent weather sorted by date
+        weather.find({}).sort({date:-1}).limit(2000).toArray(function(err, documents) {
+            if (err) res.jsonp({err:err});
+            else res.jsonp({result: documents});
+        });
+    });
+
+    app.get('/api/weather/date/range/:start/:end', function (req, res) {
+
     });
 };
